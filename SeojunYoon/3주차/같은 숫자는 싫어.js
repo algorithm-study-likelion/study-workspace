7d686f22db9b19d7ef1a18c5b0165a1367380a53
junny97 <pcohad12@gmail.com>
2023-08-25 16:44:02 +0900

    [Programmers] 같은 숫자는 싫어 - 윤서준

---
+++ SeojunYoon/3주차/같은 숫자는 싫어.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+  const answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0 || arr[i] !== arr[i - 1]) {
+      answer.push(arr[i]);
+    }
+  }
+
+  return answer;
+}
+
+const arr1 = [1, 1, 3, 3, 0, 1, 1];
+const arr2 = [4, 4, 4, 3, 3];
+
+console.log(solution(arr1)); // [1, 3, 0, 1]
+console.log(solution(arr2)); // [4, 3]
